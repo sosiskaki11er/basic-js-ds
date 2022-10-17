3816fdbb700d677aa4455f101b29b1533ac13aaa
sosiskaki11er <93476714+sosiskaki11er@users.noreply.github.com>
2022-10-17 17:20:34 +0500

    feat:binary-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,41 +7,87 @@ const { NotImplementedError } = require('../extensions/index.js');
 * using Node from extensions
 */
 class BinarySearchTree {
+	constructor() {
+		this.head = null
+	}
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	root() {
+		return this.head
+	}
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	add(data) {
+		function addNode(el, data) {
+			const newNode = new Node(data)
+			if (!el) return newNode
+			if (el.data > data) el.left = addNode(el.left, data)
+			if (el.data < data) el.right = addNode(el.right, data)
+			return el
+		}
+		this.head = addNode(this.head, data)
+	}
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	has(data) {
+		function hasNode(el, data) {
+			if (!el) return false
+			if (el.data === data) return true
+			if (el.data > data) return hasNode(el.left, data)
+			if (el.data < data) return hasNode(el.right, data)
+		}
+		return hasNode(this.head, data)
+	}
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	find(data) {
+		function findNode(el, data) {
+			if (!el) return null
+			if (el.data === data) return el
+			if (el.data > data) return findNode(el.left, data)
+			if (el.data < data) return findNode(el.right, data)
+		}
+		return findNode(this.head, data)
+	}
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	remove(data) {
+		function removeNode(el, data) {
+			if (!el) return null
+			if (el.data > data) {
+				el.left = removeNode(el.left, data)
+				return el
+			}
+			if (el.data < data) {
+				el.right = removeNode(el.right, data)
+				return el
+			}
+			if (!el.left) {
+				el = el.right
+				return el
+			}
+			if (!el.right) {
+				el = el.left
+				return el
+			}
+			let minRight = el.right
+			while (minRight.left) {
+				minRight = minRight.left
+			}
+			el.data = minRight.data
+			el.right = removeNode(el.right, minRight.data)
 
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+			return el
+		}
+		this.head = removeNode(this.head, data)
+	}
 
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	min() {
+		let el = this.head
+		while (el.left) el = el.left
+		return el.data
+	}
+
+	max() {
+		let el = this.head
+		while (el.right) el = el.right
+		return el.data
+	}
 }
 
 module.exports = {
